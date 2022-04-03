d90df2e2882b9b57c1f2a6a7501971486310692b
mahbub09 <mahabub895alam@gmail.com>
2022-04-04 05:26:25 +0600

    blogs updated

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1></h1>
-            <p>Context API is a React Structure that can solve a lot of problems.Context API make particular data available to all components, it never install a state management library in our project that will eventually cost our project performance and increase our bundle size</p>
+        <div className='text-left mx-5'>
+            <h1 className='text-xl font-semibold font-mono '>1. What is Context API?</h1>
+            <p className='mb-10'> <b>Ans. </b> Context API is a React Structure that can solve a lot of problems.Context API make particular data available to all components, it never install a state management library in our project which cost our project performance laggy and increase our bundle size. Context API offer us to exchange unique details and assists in solving prop-drilling from all levels of our application.</p>
+            <h1 className='text-xl font-semibold font-mono'>2. What is Semantic Tag?</h1>
+            <p><b>Ans. </b> Semantic Tags completed HTML  which introduces meaning to a page through specific elements that provide context as to what is in between the tags.Semantic elements clearl define their meanings and help both the browsers and developers to understand in a better way. </p>
 
         </div>
     );
