2880a7aaafd58ef5299c8ff98e752b9c7c05fe35
mahbub09 <mahabub895alam@gmail.com>
2022-04-04 05:49:19 +0600

    readme updated

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project Name: PERFUME PARADISE <br></br>
+[Live Site]()
+ 
+ <li>Made a beautifull Active Navbar</li>
+ <li>Shown All clients reviews</li>
+ <li> Two Different type of Charts</li>
+ <li>Question answer on Blogs page</li>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,8 @@ import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
-        <div>
+        <div className='flex items-center mt-3'>
+            <h1 className='font-medium font-serif underline underline-offset-6'>PERFUME PARADISE</h1>
             <nav className='container flex items-center justify-center space-x-10 font-serif font-medium text-xl'>
                 <CustomLink to='/'>Home</CustomLink>
                 <CustomLink to='/reviews'>Reviews</CustomLink>
