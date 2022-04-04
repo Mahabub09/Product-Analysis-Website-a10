619b104428444c23c8b9f5a2c5637ad1e7fb39f8
mahbub09 <mahabub895alam@gmail.com>
2022-04-04 06:37:44 +0600

    barchart

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
-    return (
-        <div>
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get('data.json')
+            .then(data => setData(data.data))
 
-        </div>
+    }, [])
+    return (
+
+        <BarChart width={500} height={300} data={data}>
+
+            <Bar dataKey="sell" fill="#8884d8" />
+            <XAxis dataKey="month"></XAxis>
+            <YAxis></YAxis>
+            <Tooltip></Tooltip>
+
+        </BarChart>
     );
 };
 
